a7509653944b98603a01825a1e7c4719050dcbfa
brsaeys <Brent.Saeys@UGent.be>
2024-11-22 21:07:35 +0100

    Update brent.py
    - Beginselen functies voor kleurcorrectie (HSV) na ruis verwijdering.
        - Histogrammen kunnen getoond worden.
    - Verwerking opgeroepen op alle video's

--- Project/brent.py
+++ Project/brent.py
@@ -2,7 +2,7 @@ import math
 import random
 import cv2
 import numpy as np
-import scipy.ndimage
+import scipy
 from matplotlib import pyplot as plt
 from scipy.io import wavfile
 import moviepy
@@ -13,31 +13,32 @@ def cumulative_histogram(image):
     cumhist = cumhist/cumhist[-1]
     return cumhist
 
-def show_results(image, result_image):
+def show_results(image,image2, nameImg="",nameImg2=""):
     fig = plt.figure(figsize=(20,15))
     ax = fig.add_subplot(321)
-    ax.set_title("Original Image")
+    ax.set_title(nameImg)
     ax0 = fig.add_subplot(322)
-    ax0.set_title("Image after Transformation")
+    ax0.set_title(nameImg2)
     ax1 = fig.add_subplot(323)
-    ax1.set_title("Histogram of Original")
+    ax1.set_title("Histogram of "+nameImg)
     ax2 = fig.add_subplot(324, sharex=ax1)
-    ax2.set_title("Histogram after Transformation")
+    ax2.set_title("Histogram of "+nameImg2)
     ax3 = fig.add_subplot(325, sharex=ax1)
-    ax3.set_title("Cumulative Histogram of Original")
+    ax3.set_title("Cumulative Histogram of "+nameImg)
     ax4 = fig.add_subplot(326, sharex=ax1)
-    ax4.set_title("Cumulative Histogram after Transformation")
+    ax4.set_title("Cumulative Histogram of "+nameImg2)
     ax1.set_xlim([0,255])
     ax.imshow(image, cmap=plt.get_cmap("gray"))
-    ax0.imshow(result_image, cmap=plt.get_cmap("gray"))
+    ax0.imshow(image2, cmap=plt.get_cmap("gray"))
     ax1.plot(cv2.calcHist([image],[0],None,[256],[0,256]))
-    ax2.plot(cv2.calcHist([result_image],[0],None,[256],[0,256]))
+    ax2.plot(cv2.calcHist([image2],[0],None,[256],[0,256]))
     ax3.plot(cumulative_histogram(image))
-    ax4.plot(cumulative_histogram(result_image))
+    ax4.plot(cumulative_histogram(image2))
     plt.show()
 
-def histogram_matching(inframe,orframe):
-    show_results(orframe, inframe)
+def histogram_matching(inframe,orframe,show=False,type=""):
+    if show:
+        show_results(orframe, inframe,type+" original",type+" before")
     ref = cumulative_histogram(orframe)
     low = cumulative_histogram(inframe)
     low_normalized = low / low.max()
@@ -51,10 +52,64 @@ def histogram_matching(inframe,orframe):
         lookup_table[i] = j - 1
 
     outframe = cv2.LUT(inframe, lookup_table)
-    show_results(orframe, outframe)
+
+    if show:
+        show_results(orframe, outframe,type+" original",type+" matched")
     return outframe
 
-def process_video(input_path,original, output_path):
+def process_frame(frame, frameOrig,show_steps=False):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    hsvOrig = cv2.cvtColor(frameOrig, cv2.COLOR_BGR2HSV)
+    ho, so, vo = cv2.split(hsvOrig)
+
+    if show_steps:
+        show_results(h, ho, "Hue", "Hue Original")
+        show_results(v, vo, "Value", "Value Original")
+        show_results(s, so, "Saturation", "Saturation Original")
+
+    #h = scipy.ndimage.maximum_filter(h,(5,5))
+    #l = scipy.ndimage.maximum_filter(l, (3,3))
+    #s = scipy.ndimage.maximum_filter(s, (5,5))
+
+    #histogram_matching(h,ho,show_steps,"Hue")
+    #histogram_matching(v, vo, show_steps, "Value")
+    #histogram_matching(s, so,show_steps,"Saturation")
+
+    #h = cv2.add(h,15)
+    #l = cv2.add(l, 80)
+    s = cv2.add(s, 60)
+
+    #s = scipy.ndimage.gaussian_filter(s,3)
+
+    #h = cv2.normalize(h, None, 0, 255, norm_type=cv2.NORM_MINMAX)
+    v = cv2.normalize(v,None,0,255,norm_type=cv2.NORM_MINMAX)
+    #s = cv2.normalize(s,None,0,255,norm_type=cv2.NORM_MINMAX)
+    #l = np.clip(l, 0, 255)
+    s = np.clip(s, 0, 255)
+    h = np.clip(h, 0, 255)
+
+    if show_steps:
+        show_results(h, ho, "Hue", "Hue Original")
+        show_results(v, vo, "Value", "Value Original")
+        show_results(s, so, "Saturation", "Saturation Original")
+
+    final_hsv = cv2.merge((h, s, v))
+    frame = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+
+    # output_0 = cv2.normalize(frame[:, :, 0],None,0,255,norm_type=cv2.NORM_MINMAX)
+    # output_1 = cv2.normalize(frame[:, :, 1], None, 0, 255, norm_type=cv2.NORM_MINMAX)
+    # output_2 = cv2.normalize(frame[:, :, 2], None, 0, 255, norm_type=cv2.NORM_MINMAX)
+    # output_0 = np.clip(output_0, 0, 255)
+    # output_1 = np.clip(output_1, 0, 255)
+    # output_2 = np.clip(output_2, 0, 255)
+
+    # Merge the color channels back together
+    # frame = cv2.merge((frame[:, :, 0], frame[:, :, 1], frame[:, :, 2])).astype(np.uint8)
+
+    return frame
+
+def process_video(input_path,original, output_path, show_steps=False, show_processed_frame=True):
     # Open the video file
     cap = cv2.VideoCapture(input_path)
     capOrig = cv2.VideoCapture(original)
@@ -74,37 +129,13 @@ def process_video(input_path,original, output_path):
         if not ret:
             break
 
-        #histogram_matching(frame[:, :, 0], frameOrig[:, :, 0])
-        #histogram_matching(frame[:, :, 1], frameOrig[:, :, 1])
-        #histogram_matching(frame[:, :, 2], frameOrig[:, :, 2])
-
-        hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        h, s, v = cv2.split(hsv)
-        v = cv2.subtract(v, 30)
-        #v = cv2.normalize(v,None,0,255,norm_type=cv2.NORM_MINMAX)
-        #s = cv2.normalize(s,None,0,255,norm_type=cv2.NORM_MINMAX)
-        s = cv2.add(s,40)
-        v = np.clip(v, 0, 255)
-        s = np.clip(s, 0, 255)
-        final_hsv = cv2.merge((h, s, v))
-        frame = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-
-        #output_0 = cv2.normalize(frame[:, :, 0],None,0,255,norm_type=cv2.NORM_MINMAX)
-        #output_1 = cv2.normalize(frame[:, :, 1], None, 0, 255, norm_type=cv2.NORM_MINMAX)
-        #output_2 = cv2.normalize(frame[:, :, 2], None, 0, 255, norm_type=cv2.NORM_MINMAX)
-        #output_0 = np.clip(output_0, 0, 255)
-        #output_1 = np.clip(output_1, 0, 255)
-        #output_2 = np.clip(output_2, 0, 255)
-
-        # Merge the color channels back together
-        #frame = cv2.merge((frame[:, :, 0], frame[:, :, 1], frame[:, :, 2])).astype(np.uint8)
-
+        frame = process_frame(frame,frameOrig,show_steps)
         # Write the processed frame
         out.write(frame)
 
-        # Optional: Display the result (comment out for faster processing)
-        cv2.imshow('Processing Video', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if show_processed_frame:
+            cv2.imshow('Processing Video', frame)
+        if show_steps or cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     # Release everything
@@ -125,9 +156,21 @@ def main():
     audio_clip.close()
     video_clip.close()"""
 
-    process_video("../DegradedVideos/archive_2017-01-07_President_Obama's_Weekly_Address.mp4","../SourceVideos/2017-01-07_President_Obama's_Weekly_Address.mp4", "output/output.mp4")
-    #process_video("../DegradedVideos/archive_2017-01-07_President_Obama's_Weekly_Address.mp4", "output/output1.mp4")
-    #process_video("../DegradedVideos/archive_2017-01-07_President_Obama's_Weekly_Address.mp4", "output/output2.mp4")
+    process_video("../DegradedVideos/archive_2017-01-07_President_Obama's_Weekly_Address.mp4",
+                  "../SourceVideos/2017-01-07_President_Obama's_Weekly_Address.mp4",
+                  "output/2017-01-07_President_Obama's_Weekly_Address.mp4")
+    process_video("../DegradedVideos/archive_20240709_female_common_yellowthroat_with_caterpillar_canoe_meadows.mp4",
+                  "../SourceVideos/20240709_female_common_yellowthroat_with_caterpillar_canoe_meadows.mp4",
+                  "output/20240709_female_common_yellowthroat_with_caterpillar_canoe_meadows.mp4")
+    process_video("../DegradedVideos/archive_Henry_Purcell__Music_For_a_While__-_Les_Arts_Florissants,_William_Christie.mp4",
+                  "../SourceVideos/Henry_Purcell__Music_For_a_While__-_Les_Arts_Florissants,_William_Christie.mp4",
+                  "output/Henry_Purcell__Music_For_a_While__-_Les_Arts_Florissants,_William_Christie.mp4")
+    process_video("../DegradedVideos/archive_Robin_Singing_video.mp4",
+                  "../SourceVideos/Robin_Singing_video.mp4",
+                  "output/Robin_Singing_video.mp4")
+    process_video("../DegradedVideos/archive_Jasmine_Rae_-_Heartbeat_(Official_Music_Video).mp4",
+                  "../SourceVideos/Jasmine_Rae_-_Heartbeat_(Official_Music_Video).mp4",
+                  "output/Jasmine_Rae_-_Heartbeat_(Official_Music_Video).mp4")
 
 if __name__ == '__main__':
     main()
